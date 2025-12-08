beeb7c940cd2106049e2851485f3f8d1560b60c4
guang229 <guang229@newschool.edu>
2025-12-08 10:35:16 -0500

    cinema again

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -5,7 +5,7 @@ const sites = [
   'https://guang229.github.io/system-within-a-mess/homepage/music/igor.html',
   'https://guang229.github.io/system-within-a-mess/homepage/music/anti.html',
   'https://guang229.github.io/system-within-a-mess/homepage/music/ctrl/ctrl.html',
-  'https://guang229.github.io/system-within-a-mess/homepage/music/cinema.html',
+  'https://guang229.github.io/system-within-a-mess/homepage/music/cinema/cinema.html',
 
 ];
 
