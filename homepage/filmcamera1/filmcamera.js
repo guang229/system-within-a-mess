6744ebe9ce867a169d976568c02634c50066b3b6
guang229 <guang229@newschool.edu>
2025-12-09 16:40:45 -0500

    filmcamera

--- homepage/filmcamera1/filmcamera.js
+++ homepage/filmcamera1/filmcamera.js
@@ -2,14 +2,14 @@ const img = document.getElementById("anim-frame");
 
 // List of frame image paths
 const frames = [
-  "/Users/gloriaguan/Desktop/cif25/Harmonic Collection/GitHub/system-within-a-mess/homepage/filmcamera1/imgs/frame1.png",
-  "/Users/gloriaguan/Desktop/cif25/Harmonic Collection/GitHub/system-within-a-mess/homepage/filmcamera1/imgs/frame2.png",
-  "/Users/gloriaguan/Desktop/cif25/Harmonic Collection/GitHub/system-within-a-mess/homepage/filmcamera1/imgs/frame3.png",
-  "/Users/gloriaguan/Desktop/cif25/Harmonic Collection/GitHub/system-within-a-mess/homepage/filmcamera1/imgs/frame4.png",
-  "/Users/gloriaguan/Desktop/cif25/Harmonic Collection/GitHub/system-within-a-mess/homepage/filmcamera1/imgs/frame5.png",
-  "/Users/gloriaguan/Desktop/cif25/Harmonic Collection/GitHub/system-within-a-mess/homepage/filmcamera1/imgs/frame6.png",
-  "/Users/gloriaguan/Desktop/cif25/Harmonic Collection/GitHub/system-within-a-mess/homepage/filmcamera1/imgs/frame7.png",
-  "/Users/gloriaguan/Desktop/cif25/Harmonic Collection/GitHub/system-within-a-mess/homepage/filmcamera1/imgs/frame8.png",
+  'homepage/filmcamera1/imgs/frame1.png',
+  'homepage/filmcamera1/imgs/frame2.png',
+  'homepage/filmcamera1/imgs/frame3.png',
+  'homepage/filmcamera1/imgs/frame4.png',
+  'homepage/filmcamera1/imgs/frame5.png',
+  'homepage/filmcamera1/imgs/frame6.png',
+  'homepage/filmcamera1/imgs/frame7.png',
+  'homepage/filmcamera1/imgs/frame8.png',
 ];
 
 let current = 0;
